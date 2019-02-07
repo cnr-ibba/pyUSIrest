b07c54830d57603ba052821b43ad36e11bdab62c
Paolo Cozzi <bunop@libero.it>
2019-02-07 11:53:29 +0100

    ":bookmark: Bump version: 0.2.1 → 0.2.2.dev0"

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/cnr-ibba/pyUSIrest',
-    version="0.2.1",
+    version="0.2.2.dev0",
     zip_safe=False,
 )
